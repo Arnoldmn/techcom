1cd1cde1b4eb3f76b16d4e826c0e388c8d699928
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-09-26 00:48:15 +0300

    Working on the appbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import Posts from './components/Posts';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import Posts from './components/Posts';
 import './App.css';
+//pages
+import home from './pages/home'
+import singlepost from './pages/singlepost'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Posts />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route path="/singlepost" component={singlepost} />
+          </Switch>
+        </Router>
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core Toolbar';
+import Button from '@material-ui/core Button';
+
 class Navbar extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <AppBar>
+                <Toolbar>
+                    <Button color="inherit">LOGO</Button>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">Notifications</Button>
+                </Toolbar>
+            </AppBar>
         )
     }
 }
